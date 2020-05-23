c2249efb880e631a23a2372877253b7a9b6cebd4
Tal Gvili <gviligvili@gmail.com>
2020-05-23 13:33:20 +0300

    Done client just need to add collapse

--- packages/client/src/pages/ApplicationsPage.tsx
+++ packages/client/src/pages/ApplicationsPage.tsx
@@ -2,14 +2,16 @@ import React, {useState} from "react";
 import './ApplicationsPage.css';
 import Settings from "../config/settings";
 import SearchBar from "../components/searchBar/SearchBar";
-import useFetch from "use-http/dist";
+import useFetch, {CachePolicies} from "use-http/dist";
 import ApplicationsTable from "../components/InstrumentsTable/ApplicationsTable";
 
 const APPLICATIONS_URL = `${Settings.get().BASE_URL}/applications/`;
 
 /** Smart component incharge of managing the instruments view */
 export default function ApplicationsPage() {
-    const { get, loading, error } = useFetch(APPLICATIONS_URL)
+    const { get, loading, error } = useFetch(APPLICATIONS_URL,{
+        'cachePolicy': CachePolicies.NETWORK_ONLY
+    })
     const [applications, setApplications] = useState([]);
 
     const onSearchSubmit = async({rating, categories, birthDate}) => {
